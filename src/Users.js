de090abf64506d14f16c84235a0305923f916263
Ravi <ravi.jadiwal@choicetechlab.com>
2021-05-12 00:31:15 +0530

    [Fragments]

--- src/Users.js
+++ src/Users.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, Fragment} from 'react';
 
 
 function Users(){
@@ -12,7 +12,21 @@ function Users(){
     ]);
     
     return(
-        <div>
+
+        // if we want not to get div tag then we can use
+        // like <> </> - this is a way of writing tag
+        // this is called fragment where our extra element tag are excluded
+        // another way is instrad of <> and </> we can use <Fragment></fragment> 
+        // this will also not show our tag in element section of inspect
+        // we will have outp like 
+        // <h1> not <div> <h1>
+        // we remove extra div or element using fragment
+        // ex now in inspect ==> element section under html tags
+        // our extra div tag will be removed only 1 main div of app will come
+        // not div of this component as we replaced it using fragment
+        // so fragment is used to remove extra tags or elements
+        // either use fragment or empty <>
+        <Fragment>
             <h1>
                 User Listing is here
             </h1>
@@ -34,7 +48,7 @@ function Users(){
                     </div>
                 )
             } */}
-        </div>
+        </Fragment>
     )
 }
 
